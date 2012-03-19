a57454c894bf3f5e369abe94c6c8cff629046a6c
Sebastien DIJOUX <iknot974@gmail.com>
2012-03-19 02:18:15 +0100

    ajout du dossier projet

---
+++ jMoodleXML/src/main/java/DCLL_Groupe9/jMoodleXML/App.java
@@ -0,0 +1,13 @@
+package DCLL_Groupe9.jMoodleXML;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hellffo World!" );
+    }
+}
